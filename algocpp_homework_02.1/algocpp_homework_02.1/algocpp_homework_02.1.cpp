43a0f9c66d5fbfe0e43d65d6e1a6a3eba0f078e7
Andrey Partyanskiy <andreypartyanskiy@gmail.com>
2023-05-29 19:39:22 +0300

    First commit

---
+++ algocpp_homework_02.1/algocpp_homework_02.1/algocpp_homework_02.1.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+unsigned int fib(unsigned int n) 
+{
+    if (n == 2 || n == 1) return 1;
+    if (n == 0) return 0;
+    return fib (n - 2) + fib(n - 1);
+}
+
+int main()
+{
+    std::cout << "fib = " << fib(6) << std::endl;
+    return 0;
+}                                              
---
+++ algocpp_homework_02.2/algocpp_homework_02.2/algocpp_homework_02.2.cpp
@@ -0,0 +1,28 @@
+﻿// algocpp_homework_02.2.cpp 
+
+#include <iostream>
+
+unsigned int fib(int n, unsigned int* arr) 
+{
+    if (n == 1 || n == 2) 
+    {
+        arr[n] = 1;
+        return 1;
+    };
+    if (n == 0) return 0;
+    if (arr[n] != 0) return arr[n];
+    else
+    {
+        arr[n]= fib(n - 2, arr) + fib(n - 1, arr);
+    }
+    return fib(n - 2, arr) + fib(n - 1, arr);   
+}
+
+int main()
+{
+    int n = 8;
+    unsigned int* arr = new unsigned int [n+1] {};
+    std::cout << "fib"<<" [" << n << "] = " << fib(n, arr) << std::endl;
+    delete [] arr;
+    return 0;
+}
